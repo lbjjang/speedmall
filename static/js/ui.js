4c5b9890f1ac6116542784172a92a3280d14e620
shinwy25 <shinwy25@pulipinc.com>
2021-06-30 17:52:07 +0900

    guide.정리
    .

--- static/js/ui.js
+++ static/js/ui.js
@@ -10,10 +10,41 @@ function maxLengthCheck(object){
 	}    
 }
 
+function fillWidth(elem, timer, limit) {
+	if (!timer) { timer = 2500; }	
+	if (!limit) { limit = 100; }
+	var width = 1;
+	var id = setInterval(frame, timer / 100);
+		function frame() {
+		if (width >= limit) {
+			clearInterval(id);
+		} else {
+			width++;
+			elem.style.width = width + '%';
+		}
+	}
+};
 
+function toast(msg, timer) {
+	if (!timer) { timer = 2500; }
+	var $elem = $("<div class='toastWrap'><span class='toast'>" + msg + "</span></div>");
+	$("#toast").append($elem); //top = prepend, bottom = append
+	$elem.slideToggle(100, function() {
+		$('.timerWrap', this).first().outerWidth($elem.find('.toast').first().outerWidth() - 10);
+		if (!isNaN(timer)) {
+			fillWidth($elem.find('.timer').first()[0], timer);
+			setTimeout(function() {
+				$elem.fadeOut(function() {
+					$(this).remove();
+				});
+			}, timer);			
+		}
+	});
+}
 
 
 $(document).ready(function () {
+	$('body').append('<div id="toast"></div>');
 	//슬라이드 열고 닫기
 	$('.slideCon .top a').click(function(e){
 		e.preventDefault();
@@ -45,6 +76,15 @@ $(document).ready(function () {
 		setTimeout(function(){
 			$('.btn.reset a').removeClass('spin');		
 		}, 1000);
+	});
+
+	//툴팁 모션
+	$('.btn.tooltip a').click(function(){
+		$(this).next('.tooltipBox').fadeIn();
+	});
+
+	$('.tooltipBox .tooltipClose').click(function(){
+		$(this).parent().fadeOut();
 	});
 
 	//찜하기 버튼
